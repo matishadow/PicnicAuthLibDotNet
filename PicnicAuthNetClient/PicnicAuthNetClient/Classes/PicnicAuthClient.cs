d5f4d1b68f61c256b00f4bd81aaeedd72c5f3633
matishadow <matishadow@gmail.com>
2017-11-28 21:18:03 +0100

    Add AddCompany method

---
+++ PicnicAuthNetClient/PicnicAuthNetClient/Classes/PicnicAuthClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RestSharp;
+
+namespace PicnicAuthNetClient.Classes
+{
+    public class PicnicAuthClient
+    {
+        private readonly Uri baseEndpoint;
+        private readonly string apiKey;
+        private readonly IRestClient restClient;
+
+        private const string AuthenticationHeaderName = "Authorization";
+        private const string AuthenticationHeaderValueFormat = "Bearer {0}";
+
+
+        public PicnicAuthClient(Uri baseEndpoint, string apiKey)
+        {
+            this.baseEndpoint = baseEndpoint;
+            this.apiKey = apiKey;
+            restClient = new RestClient(baseEndpoint);
+        }
+
+        public IRestResponse AddCompany(string email, string userName, string password)
+        {
+            var request = new RestRequest("Companies", Method.POST);
+
+            request.AddJsonBody(new
+            {
+                Email = email,
+                UserName = userName,
+                Password = password,
+                ConfirmPassword = password
+            });
+
+            return restClient.Execute(request);
+        }
+
+        private void AddAuthorizationHeader(IRestRequest request)
+        {
+            request?.AddHeader(AuthenticationHeaderName, string.Format(AuthenticationHeaderValueFormat, apiKey));
+        }
+    }
+}
